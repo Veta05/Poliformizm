b0ff4e35b7832945004d309930aa2a1f5bf9ffdd
Jelizaveta Aia <jelizaveta05@gmail.com>
2022-01-31 06:31:38 +0200

    Добавьте файлы проекта.

---
+++ Poliformizm/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliformizm
+{
+    abstract class Person
+    {
+        string name;   //Фамилия
+        public void SetName()        // Метод запроса фамилии
+        { Console.Write("Lastname : "); name = Console.ReadLine(); }
+        public void GetName()      // Метод вывода фамилии 
+        { Console.WriteLine(" Mr " + name); }
+        //реализация следующих методов будет определена в потомках      
+        public abstract void SetData();   // Метод запроса данных
+        public abstract bool SetStat();   //  Метод определения выдающейся личности
+    }
+    class Student : Person   //производный класс  Студент
+    {
+        double ball;     //средний балл студента
+        public override void SetData()     // Метод запроса данных по студенту
+        {
+            Console.WriteLine("Student: ");
+            base.SetName();    // вызов метода базового класса Person
+            Console.Write("Avarage mark: ");
+            string s;
+            s = Console.ReadLine();
+            ball = Convert.ToDouble(s);
+        }
+        public override bool SetStat()   // Метод определения выдающегося студента 
+        { if (ball > 4.8) { base.GetName(); Console.Write("Avarage mark=" + ball); return true; } else return false; }
+    }
+    class Teach : Person    //производный класс  Преподаватель
+    {
+        int kol;          // количество публикаций преподавателя
+        public override void SetData()   // Метод запроса данных по преподавателю
+        {
+            Console.WriteLine("Teacher");
+            base.SetName();     // вызов метода базового класса Person
+            Console.Write("Number of publications:");
+            string s;
+            s = Console.ReadLine();
+            kol = Convert.ToInt32(s);
+        }
+        public override bool SetStat()  // Метод определения выдающегося преподавателя 
+        { if (kol > 10) { base.GetName(); Console.Write("Number of publications=" + kol); return true; } else return false; }
+    }
+}
+
---
+++ Poliformizm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Poliformizm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] ob = new Person[5];   //массив ссылок
+            int n = 0; string s, s1;
+            do
+            {
+                Console.WriteLine("Student(s) / Teacher(t)?");
+                s = Console.ReadLine();
+                if (s == "s") ob[n] = new Student(); // создаем объект класса Студент
+                if (s == "t") ob[n] = new Teach();   // создаем объект класса Преподаватель
+                ob[n].SetData();        // вызов метода соответствующего производного класса
+                n++;
+                Console.WriteLine("y/n?");
+                s1 = Console.ReadLine();
+            } while (s1 == "y");
+            for (int i = 0; i < n; i++)   // проверка на выдающуюся личность
+            {
+                if (ob[i].SetStat()) { Console.WriteLine(" An outstanding person! "); }
+            }
+            Console.ReadLine();
+        }
+    }
+}
